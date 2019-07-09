0c975c337150fa308b1cde5a49d66de37bad9a47
Sander Willekens <sander.willekens@hotmail.com>
2019-07-09 13:12:04 +0200

    Add project files.

---
+++ RekenmachineLibrary/Berekening.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RekenmachineLibrary
+{
+    public class Berekening
+    {
+        public static double MaakBerekening(double getal1,double getal2,char operatie)
+        {
+            double resultaat;
+            switch (operatie)
+            {
+                case '+':
+                    resultaat = getal1 + getal2;
+                    break;
+                case '-':
+                    resultaat = getal1 - getal2;
+                    break;
+                case '*':
+                    resultaat = getal1 * getal2;
+                    break;
+                case '/':
+                    resultaat = getal1 / getal2;
+                    break;
+                case '^':
+                    resultaat = Math.Pow(getal1, getal2);
+                    break;
+                default:
+                    resultaat = 0;
+                    break;
+            }
+            return resultaat;
+        }
+    }
+}
